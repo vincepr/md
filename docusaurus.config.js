732d39c67e3376f3616bb5d52c2a7bc28f1e47c1
vincepr <vinceprinceking@gmail.com>
2023-05-11 08:30:57 +0200

    csharp

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -141,7 +141,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages:["rust", "C#"],
+        additionalLanguages:["rust", "csharp"],
       },
     }),
 
